bf916bb71cac56fe8bddd112a94f7e468adf67aa
Igor Moreno <igormoreno@gmail.com>
2022-04-26 18:25:19 +0200

    documentation

--- src/model/Game.java
+++ src/model/Game.java
@@ -7,10 +7,16 @@ public class Game {
     private Player currentPlayer;
     
     
+    /**
+     * Create a game with default starting player.
+     */
     public Game() {
         this(Player.CROSS);
     }
     
+    /**
+     * Create a game choosing a starting player.
+     */
     public Game(Player startingPlayer) {
         board = new Board();
         currentPlayer = startingPlayer;
